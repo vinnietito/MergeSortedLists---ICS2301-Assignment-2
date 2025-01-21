bc4f34f65939b6e6979695f063b6cd958afc3235
vinnietito <vincentnjuguna69@gmail.com>
2025-01-21 07:12:04 +0300

    Commits

---
+++ merge_sorted_lists.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+// Function to merge two sorted arrays
+void mergeSortedLists(int list1[], int size1, int list2[], int size2, int mergedList[]) {
+    int i = 0, j = 0, k = 0;
+
+    // Merge elements from both arrays in sorted order
+    while (i < size1 && j < size2) {
+        if (list1[i] <= list2[j]) {
+            mergedList[k++] = list1[i++];
+        } else {
+            mergedList[k++] = list2[j++];
+        }
+    }
+
+    // Add remaining elements from list1 (if any)
+    while (i < size1) {
+        mergedList[k++] = list1[i++];
+    }
+
+    // Add remaining elements from list2 (if any)
+    while (j < size2) {
+        mergedList[k++] = list2[j++];
+    }
+}
+
+int main() {
+    // Sample input arrays
+    int list1[] = {1, 3, 5, 7};
+    int list2[] = {2, 4, 6, 8};
+    int size1 = sizeof(list1) / sizeof(list1[0]);
+    int size2 = sizeof(list2) / sizeof(list2[0]);
+    int mergedList[size1 + size2];
+
+    // Merge the lists
+    mergeSortedLists(list1, size1, list2, size2, mergedList);
+
+    // Print the merged list
+    printf("Merged Sorted List: ");
+    for (int i = 0; i < size1 + size2; i++) {
+        printf("%d ", mergedList[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
