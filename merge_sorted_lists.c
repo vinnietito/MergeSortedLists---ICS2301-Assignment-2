dc5d13b8d45145680f4de87067e4701397f19c62
vinnietito <vincentnjuguna69@gmail.com>
2025-01-22 07:12:24 +0300

    Commits

--- merge_sorted_lists.c
+++ merge_sorted_lists.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
-void mergeSortedLists(int list1[], int size1, int list2[], int size2, int mergedList[]) {
+void mergeSortedLists(int list1[], int size1, int list2[], int size2, int mergedList[])
+{
     int i = 0, j = 0, k = 0;
 
-    while (i < size1 && j < size2) {
+    while (i < size1 && j < size2)
+	{
         if (list1[i] <= list2[j]) {
             mergedList[k++] = list1[i++];
         } else {
